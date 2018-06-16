2d57c141d272d2f7988b3c2df6eaa325f6e4d215
melingiusti <melingiusti@melingiusti-VirtualBox>
2018-06-16 11:03:11 +0200

    Commit 20.

---
+++ HelloWorld/MAIN.h
@@ -0,0 +1,17 @@
+/*
+ * MAIN.h
+ *
+ *  Created on: 16 giu 2018
+ *      Author: melingiusti
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define true 1
+#define false 0
+
+void multiplication(int a, int b);
+
+
+#endif /* MAIN_H_ */
